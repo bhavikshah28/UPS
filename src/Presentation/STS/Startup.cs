aea4f6161a6cfe29d026e35f627d0439d0d8e932
bhavikshah28 <bhavikshah268@gmail.com>
2023-10-18 13:01:00 +0530

    changes for STS

---
+++ src/Presentation/STS/Startup.cs
@@ -0,0 +1,56 @@
+﻿using UPS.Application;
+using UPS.Common;
+using UPS.Infrastructure;
+using UPS.Infrastructure.Identity;
+using UPS.STS.Seed;
+using Duende.IdentityServer.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UPS.STS
+{
+    public class Startup
+    {
+        public static IConfiguration Configuration { get; set; }
+        public IWebHostEnvironment HostingEnvironment { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            HostingEnvironment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IProfileService, CustomProfileService>();
+            services.AddTransient<IIdentitySeedData, IdentitySeedData>();
+
+            services
+                .AddApplication()
+                .AddInfrastructure(Configuration, HostingEnvironment)
+                .AddHealthChecks()
+                .AddDbContextCheck<IdentityServerDbContext>();
+            
+            services.AddStsServer(Configuration, HostingEnvironment);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseInfrastructure(HostingEnvironment);
+            app.UseRouting();
+            app.UseCors(Constants.DefaultCorsPolicy);
+            app.UseAuthentication();
+            app.UseIdentityServer();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
