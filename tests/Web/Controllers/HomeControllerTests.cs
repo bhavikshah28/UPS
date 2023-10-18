089930dcc75173faadf909ed6e9d3a25b959c35b
bhavikshah28 <bhavikshah268@gmail.com>
2023-10-18 12:56:31 +0530

    changes for dev

---
+++ tests/Web/Controllers/HomeControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace UPS.Web.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void DummyTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
